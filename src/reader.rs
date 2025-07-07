6e311f2039d35ff04581a4e7c07a9bb76b551589
Bartosz Koncewicz <bkoncewicz@smartadserver.com>
2025-07-07 10:48:38 +0200

    reader adjust

--- src/reader.rs
+++ src/reader.rs
@@ -1,4 +1,4 @@
-﻿use crate::kafka_reader_args::ReadArgs;
+﻿use crate::arguments::ReadArgs;
 use crate::profile::ProfileData;
 use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
 use serde::Deserialize;
@@ -7,10 +7,11 @@ pub fn read_data(profiles: &Vec<ProfileData>, read_args: &ReadArgs) {
     println!("command: Read");
     println!("{:#?}", read_args);
     println!("{:#?}", profiles);
+    
+    let mode = Mode::Utf8;
 
     let profile = if let Some(p) = read_args.profile.as_deref() {
-        let xxx = profiles.iter().find(|x| x.get_profile_name().eq(p));
-        xxx
+        profiles.iter().find(|x| x.get_profile_name().eq(p))
     } else {
         None
     };
@@ -47,7 +48,10 @@ pub fn read_data(profiles: &Vec<ProfileData>, read_args: &ReadArgs) {
                 println!("{:?}", m);
 
                 match str::from_utf8(m.value) {
-                    Ok(v) => println!("{:?}", v),
+                    Ok(v) => match mode {
+                        Mode::Utf8 => println!("{}", v),
+                        Mode::Proto => todo!(),
+                    },
                     Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
                 };
             }
@@ -61,4 +65,9 @@ pub fn read_data(profiles: &Vec<ProfileData>, read_args: &ReadArgs) {
 struct Thing {
     name: String,
     email: String,
+}
+
+enum Mode{
+    Utf8,
+    Proto
 }
